7a8f61bb8c681c2ef41d6a8432a156aa7ad0577a
박성제 <157565587+Parksungje@users.noreply.github.com>
2024-06-13 00:43:32 +0900

    SOLO

--- Solo/Assets/02 Script/Core/EnemyM.cs
+++ Solo/Assets/02 Script/Core/EnemyM.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyM : MonoBehaviour
+public class EnemtM : MonoBehaviour
 {
-    
+    public GameObject[] EnemyObjs;
+    public Transform[] spawnPoint;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(1f, 5f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(1, 1);
+        int ranPoint = Random.Range(0, 6);
+        Instantiate(EnemyObjs[ranEnemy],
+                    spawnPoint[ranPoint].position,
+                    spawnPoint[ranPoint].rotation);
+    }
 }
